4b18cf68e4be0d3c2b9527180adaca355bbfc4ab
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-06 23:41:12 +0000

    Passing unit test by adding instance variable capacity and attribute reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,10 @@
 require_relative 'bike'
 class DockingStation
   DEFAULT_CAPACITY = 20
+  attr_reader :capacity
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def release_bike
@@ -18,7 +20,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= DEFAULT_CAPACITY 
+    @bikes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
