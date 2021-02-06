99cd455a889fd2245336aff58ea5c1d38297e537
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-06 23:36:54 +0000

    Unit test for new user story

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,4 +29,7 @@ describe DockingStation do
         expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
+  it 'has a default capacity' do
+   expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+ end
 end
