410289ccbbac30566aa8d10a7c8ff76330795627
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-06 18:33:38 +0000

    Passing unit tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,16 @@
 require_relative 'bike'
 class DockingStation
-   attr_reader :bike
+  def initialize
+   @bikes = []
+ end
 
-  def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
-  end
+ def release_bike
+   fail 'No bikes available' if @bikes.empty?
+   @bikes.pop
+ end
 
-
-  def dock(bike)
-    fail 'Docking station full' if @bike
-    @bike = bike
-  end
-
-  def bike
-    @bike
-  end
-
+ def dock(bike)
+   fail 'Docking station full' if @bikes.count >= 20
+   @bikes << bike
+ end
 end
