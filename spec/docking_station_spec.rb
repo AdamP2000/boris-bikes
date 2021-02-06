583ec648bedb5c17f1d99e312d1d6f1bd08ffefc
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-06 16:16:05 +0000

    Updating unit test for release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,12 @@
 require 'docking_station'
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  describe '#release_bike' do
+    it 'releases a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+    end
+  end
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
