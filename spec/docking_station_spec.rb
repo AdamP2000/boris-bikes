88706e1c52cf236d31ac60c8ab797f8a7f748274
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-07 01:03:34 +0000

    New unit tests for default capacity and changing capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -32,4 +32,21 @@ describe DockingStation do
   it 'has a default capacity' do
    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
  end
+ describe 'initialization' do
+  it 'has a variable capacity' do
+    docking_station = DockingStation.new(50)
+    50.times { docking_station.dock Bike.new }
+    expect{ docking_station.dock Bike.new }.to raise_error 'Docking station full'
+  end
+end
+describe 'initialization' do
+  subject { DockingStation.new }
+  let(:bike) { Bike.new }
+  it 'defaults capacity' do
+    described_class::DEFAULT_CAPACITY.times do
+      subject.dock(bike)
+    end
+    expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+  end
+end
 end
