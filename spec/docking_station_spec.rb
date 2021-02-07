a5317bf46fde708836262bf517dad970749d6118
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-07 14:31:26 +0000

    Unit test for not releasing broken bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -49,4 +49,10 @@ describe 'initialization' do
     expect{ subject.dock(bike) }.to raise_error 'Docking station full'
   end
 end
+it "does not release a broken bike" do
+  bike = Bike.new
+  bike.report_broken
+  subject.dock(bike)
+  expect{ subject.release_bike }.to raise_error 'No bikes available'
+end
 end
